25f6f4038d2ff38c22fe0d675fa28cd4aec45d64
WilbertFE <wilbertbernadi699@gmail.com>
2024-06-27 13:53:13 +0700

    Make camera button

--- frontend/src/pages/profile/Banner.tsx
+++ frontend/src/pages/profile/Banner.tsx
@@ -1,7 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Link } from "react-router-dom";
 import { UserInterface } from "@/types";
-import { Settings } from "lucide-react";
+import { Camera, Settings } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface BannerProps {
   user: UserInterface;
@@ -9,6 +11,13 @@ interface BannerProps {
 }
 
 export function Banner({ user, owner }: BannerProps) {
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    if (target.files) {
+      console.log(target.files[0]);
+    }
+  };
+
   return (
     <div className="relative">
       <div className="bg-secondary-config/10 h-[120px] rounded-lg"></div>
@@ -18,11 +27,24 @@ export function Banner({ user, owner }: BannerProps) {
           {user.displayName}
         </h1>
       </div>
-      <div>
-        <Avatar className="absolute w-24 h-24 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <div className="absolute w-24 h-24 -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+        <Avatar className="w-full h-full">
           <AvatarImage src={user.image || "https://github.com/shadcn.png"} />
           <AvatarFallback>TB</AvatarFallback>
         </Avatar>
+        <Label
+          htmlFor="profile_image"
+          className="absolute p-1 rounded-full bg-black/50 -right-1 bottom-1"
+        >
+          <Camera size={24} color="#fff" />
+        </Label>
+        <Input
+          onChange={handleImageChange}
+          type="file"
+          accept="image/*"
+          id="profile_image"
+          className="hidden"
+        />
       </div>
       {owner && (
         <div className="absolute right-0 top-3 text-light-config">
