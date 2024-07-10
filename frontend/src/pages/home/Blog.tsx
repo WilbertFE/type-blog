cf182473748a66a808877b3d486078910cab89d5
WilbertFE <wilbertbernadi699@gmail.com>
2024-07-10 09:17:52 +0700

    Comment UI

--- frontend/src/pages/home/Blog.tsx
+++ frontend/src/pages/home/Blog.tsx
@@ -10,7 +10,16 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useUser } from "@/hooks/useUser";
 import { Link } from "react-router-dom";
 import { BlogInterface } from "@/types";
-import { Heart, MessageSquare } from "lucide-react";
+import { ChevronDown, MessageSquare, ThumbsDown, ThumbsUp } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 
 export function Blog(props: { blog: BlogInterface }) {
   const { blog } = props;
@@ -46,14 +55,73 @@ export function Blog(props: { blog: BlogInterface }) {
           <CardFooter>
             <div>
               <div className="flex mb-2 gap-x-4">
-                <div className="flex gap-x-1">
-                  <Heart />
-                  <span>20</span>
+                <div className="flex cursor-pointer gap-x-1">
+                  <ThumbsUp />
+                  <span>0</span>
                 </div>
-                <div className="flex gap-x-1">
-                  <MessageSquare />
-                  <span>5</span>
-                </div>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <div className="flex cursor-pointer gap-x-1">
+                      <MessageSquare />
+                      <span>0</span>
+                    </div>
+                  </DialogTrigger>
+                  <DialogContent className="sm:max-w-xl">
+                    <DialogHeader>
+                      <DialogTitle>Comments 1.0K</DialogTitle>
+                    </DialogHeader>
+                    <div className="flex flex-col gap-4 py-4">
+                      <div className="flex gap-x-4">
+                        <Avatar>
+                          <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
+                          />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col">
+                          <h3>@wilbert</h3>
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Reiciendis, sint.
+                          </p>
+                          <div className="flex mt-2 gap-x-3">
+                            <div className="flex gap-x-1">
+                              <ThumbsUp strokeWidth={1} />
+                              <span>0</span>
+                            </div>
+                            <div className="flex gap-x-1">
+                              <ThumbsDown strokeWidth={1} />
+                              <span>0</span>
+                            </div>
+                          </div>
+                          <div className="flex items-center self-start mt-2 cursor-pointer">
+                            <ChevronDown className="text-blue-600" />
+                            <span className="font-medium text-blue-600">
+                              1 replies
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <DialogFooter className="block">
+                      <div className="flex">
+                        <Avatar>
+                          <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
+                          />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <Input
+                          type="text"
+                          placeholder="Add a comment"
+                          className="border-t-0 border-b border-x-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:border-x-0 focus-visible:border-t-0 focus-visible:ring-offset-0"
+                        />
+                      </div>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </div>
               <p className="text-sm text-slate-400">
                 {new Date(user.updatedAt || "").toDateString()}
